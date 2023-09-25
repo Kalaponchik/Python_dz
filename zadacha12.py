d0691dece86cdbe5fbdc513e09f14eaf117560a2
Merey <mereykz1990@gmail.com>
2023-09-25 22:46:34 +0600

    first commit

---
+++ zadacha12.py
@@ -0,0 +1,8 @@
+x=12
+y=20
+for a in range(1000):
+    for b in range(1000):
+        if a+b==x:
+             if a*b==y:
+                 print(a,b)
+             
---
+++ zadacha14.py
@@ -0,0 +1,5 @@
+n=int(input())
+res=1
+while res<=n:
+    print(res)
+    res=res*2
